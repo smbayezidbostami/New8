587761bc229b66c0046378cecb13501ebb06dc87
“smbayezidbostami” <bayezid.eece@gmail.com>
2021-02-13 01:23:41 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j float32
+	fmt.Printf("Enter your first number :")
+	fmt.Scan(&i)
+	fmt.Printf("Enter your second number :")
+	fmt.Scan(&j)
+	add := i + j+
+	sub := i - j-
+	mult := i * j*
+	div := i / j/
+	fmt.Println("addition :=", add)
+	fmt.Println("subtraction :=", sub)
+	fmt.Println("multiply :=", mult)
+	fmt.Println("divid :=", div)
+}
